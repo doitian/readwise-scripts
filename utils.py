e719b2ea1ae401c2e19f0365c2138637e9b70e6c
ian <me@iany.me>
2023-12-11 19:15:55 +0800

    chore: format utils

--- utils.py
+++ utils.py
@@ -6,22 +6,25 @@ import time
 
 
 def is_concatenating(entry):
-    return 'note' in entry and entry['note'] != '' and \
-        entry['note'].split()[0] in ['.c1', '.c2', '.c3', '.c4', '.c5']
+    return (
+        "note" in entry
+        and entry["note"] != ""
+        and entry["note"].split()[0] in [".c1", ".c2", ".c3", ".c4", ".c5"]
+    )
 
 
 def concatenate_highlights(highlights):
     result = highlights[0]
-    result['text'] = ' '.join(entry['text'] for entry in highlights)
+    result["text"] = " ".join(entry["text"] for entry in highlights)
     notes = []
     for entry in highlights:
-        entry_note = entry['note'][3:].strip()
-        if entry_note != '':
+        entry_note = entry["note"][3:].strip()
+        if entry_note != "":
             notes.append(entry_note)
     if len(notes) > 0:
-        result['note'] = "\n".join(notes)
+        result["note"] = "\n".join(notes)
     else:
-        del result['note']
+        del result["note"]
 
     return result
 
@@ -30,7 +33,7 @@ def squash_concatenating_highlights(highlights):
     pending_spans = []
     for entry in highlights:
         if is_concatenating(entry):
-            if entry['note'].split()[0] == '.c1':
+            if entry["note"].split()[0] == ".c1":
                 if len(pending_spans) > 0:
                     yield concatenate_highlights(pending_spans)
                 pending_spans = [entry]
@@ -47,50 +50,48 @@ def squash_concatenating_highlights(highlights):
 
 
 def add_tags(highlights, ids, token, user_agent):
-    for (entry, id) in zip(highlights, ids):
-        if 'note' in entry and entry['note'].startswith('.'):
-            for tag in entry['note'].splitlines()[0].split():
+    for entry, id in zip(highlights, ids):
+        if "note" in entry and entry["note"].startswith("."):
+            for tag in entry["note"].splitlines()[0].split():
                 req = Request(
-                    f'https://readwise.io/api/v2/highlights/{id}/tags/',
+                    f"https://readwise.io/api/v2/highlights/{id}/tags/",
                     headers={
-                        'Authorization': f'Token {token}',
-                        'Content-Type': 'application/json',
-                        'Accept': 'application/json',
-                        'User-Agent': user_agent
+                        "Authorization": f"Token {token}",
+                        "Content-Type": "application/json",
+                        "Accept": "application/json",
+                        "User-Agent": user_agent,
                     },
-                    data=json.dumps({'name': tag[1:]}).encode('utf-8'),
-                    method='POST',
+                    data=json.dumps({"name": tag[1:]}).encode("utf-8"),
+                    method="POST",
                 )
                 urlopen(req)
 
 
 def create_highlights(highlights, token=None, user_agent=None):
     if token is None:
-        token = os.environ['READWISE_TOKEN']
+        token = os.environ["READWISE_TOKEN"]
     if user_agent is None:
-        user_agent = os.environ['USER_AGENT']
+        user_agent = os.environ["USER_AGENT"]
 
     highlights = list(squash_concatenating_highlights(highlights))
 
     req = Request(
-        'https://readwise.io/api/v2/highlights/',
+        "https://readwise.io/api/v2/highlights/",
         headers={
-            'Authorization': f'Token {token}',
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-            'User-Agent': user_agent
+            "Authorization": f"Token {token}",
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "User-Agent": user_agent,
         },
-        data=json.dumps({'highlights': highlights}).encode('utf-8'),
-        method='POST',
+        data=json.dumps({"highlights": highlights}).encode("utf-8"),
+        method="POST",
     )
     resp = urlopen(req)
 
-    items = json.loads(resp.read().decode('utf-8'))
-    if len(items) == 1 and len(items[0]['modified_highlights']) == len(highlights):
+    items = json.loads(resp.read().decode("utf-8"))
+    if len(items) == 1 and len(items[0]["modified_highlights"]) == len(highlights):
         try:
-            add_tags(highlights, items[0]
-                     ['modified_highlights'], token, user_agent)
+            add_tags(highlights, items[0]["modified_highlights"], token, user_agent)
         except HTTPError:
             time.sleep(5)
-            add_tags(highlights, items[0]
-                     ['modified_highlights'], token, user_agent)
+            add_tags(highlights, items[0]["modified_highlights"], token, user_agent)
