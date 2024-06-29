b2c7b8746a39b89f7c32be473dfdffbe68676d13
ian <me@iany.me>
2024-06-29 11:03:11 +0800

    kindle: support zh

--- kindle-html-to-readwise.py
+++ kindle-html-to-readwise.py
@@ -2,11 +2,22 @@
 
 import fileinput
 import json
-import os
 
 from bs4 import BeautifulSoup
 
 import utils
+
+EN = {}
+ZH = {
+    "Highlight": "标注",
+    " - Location ": " -  位置 ",
+    "Note": "笔记",
+    " · Location ": " >  位置 ",
+}
+
+
+def _(key):
+    return ZH.get(key, key)
 
 
 def collect_highlights(soup):
@@ -16,29 +27,38 @@ def collect_highlights(soup):
         "source_type": "Kindle",
         "category": "books",
     }
-    if "SOURCE_URL" in os.environ:
-        article_template["source_url"] = os.environ["SOURCE_URL"]
+    source_url_dom = soup.find(class_="sourceURL")
+    if source_url_dom is not None:
+        article_template["source_url"] = source_url_dom.get_text().strip()
 
     result = []
+    last_chapter = None
     for div in soup.select(".bodyContainer > div"):
         if "sectionHeading" in div["class"]:
             article = article_template.copy()
             article["text"] = div.get_text().strip()
             article["note"] = ".h1"
             result.append(article)
-        elif "noteHeading" in div["class"] and "Highlight" in div.get_text():
+        elif "noteHeading" in div["class"] and _("Highlight") in div.get_text():
             article = article_template.copy()
             siblings = div.find_next_siblings("div", limit=3)
             if len(siblings) < 1 or "noteText" not in siblings[0]["class"]:
                 raise RuntimeError(f"{div.get_text().strip()} has no noteText")
             article["text"] = siblings[0].get_text().strip()
-            heading_parts = div.get_text().strip().split(" - Location ")
+            heading_parts = div.get_text().strip().split(_(" - Location "))
             if len(heading_parts) == 2:
                 article["text"] = f'{article["text"]} (Loc {heading_parts[1]})'
             else:
-                heading_parts = div.get_text().strip().split(" · Location ")
+                heading_parts = div.get_text().strip().split(_(" · Location "))
                 if len(heading_parts) == 2:
                     article["text"] = f'{article["text"]} (Loc {heading_parts[1]})'
+                    chapter_parts = heading_parts[0].split(") - ", maxsplit=1)
+                    if len(chapter_parts) == 2 and chapter_parts[1] != last_chapter:
+                        last_chapter = chapter_parts[1]
+                        chapter_article = article_template.copy()
+                        chapter_article["text"] = chapter_parts[1]
+                        chapter_article["note"] = ".h2"
+                        result.append(chapter_article)
 
             result.append(article)
             if len(siblings) == 2:
@@ -56,7 +76,7 @@ def collect_highlights(soup):
                 raise RuntimeError(
                     f"{div.get_text().strip()} has unknown following siblings"
                 )
-            if len(siblings) > 1 and "Note" in siblings[1].get_text():
+            if len(siblings) > 1 and _("Note") in siblings[1].get_text():
                 article["note"] = siblings[2].get_text().strip()
 
             color_span = div.find("span")
@@ -64,9 +84,10 @@ def collect_highlights(soup):
                 color_span is not None
                 and "highlight" in " ".join(color_span["class"])
                 and color_span.get_text().strip() != "yellow"
+                and color_span.get_text().strip() != "黄色"
             ):
                 article["note"] = add_tag(
-                    article.get("note"), "." + color_span.get_text().strip()
+                    article.get("note"), "." + _(color_span.get_text().strip())
                 )
 
     return result
